c1a3accf2ab40976af81fae0931d9cd964433a1d
HyunSuk <lha3421@gmail.com>
2022-04-27 11:25:15 +0900

    ADD : 브랜치 이동위해서 임시저장

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,7 +1,42 @@
 import React from "react";
+import "./Cart.scss";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <div className="cartContainer">
+      <h1>장바구니</h1>
+      <div className="cartWrapper">
+        <div className="cartContent">
+          <div>
+            <input type="checkbox" />
+            <button>전체선택</button>
+          </div>
+          <div>
+            <button>선택삭제</button>
+          </div>
+        </div>
+        <div className="cartPayment">
+          <div className="paymentResult">
+            <div className="productPrice">
+              <span>총 상품금액</span>
+              <span>0원</span>
+            </div>
+            <div className="deliveryPrice">
+              <span>배송비</span>
+              <span>+0원</span>
+            </div>
+            <div className="totalPrice">
+              <span>결제예상금액</span>
+              <span>0원</span>
+            </div>
+          </div>
+          <div>
+            <button>0원 주문하기</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
