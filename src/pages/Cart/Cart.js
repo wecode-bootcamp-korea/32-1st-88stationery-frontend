2b62452a2d8355d0c1fe067bbb5585853e918402
HyunSuk <lha3421@gmail.com>
2022-04-27 17:54:55 +0900

    ADD : cart레이아웃 및 스타일링 완성

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -4,16 +4,45 @@ import "./Cart.scss";
 const Cart = () => {
   return (
     <div className="cartContainer">
-      <h1>장바구니</h1>
+      <h1 className="cartTitle">장바구니</h1>
       <div className="cartWrapper">
         <div className="cartContent">
-          <div>
-            <input type="checkbox" />
-            <button>전체선택</button>
+          <div className="cartContentHeader">
+            <div>
+              <input type="checkbox" />
+              <button>전체선택</button>
+            </div>
+            <div>
+              <button>선택삭제</button>
+            </div>
           </div>
-          <div>
-            <button>선택삭제</button>
-          </div>
+          <ul className="cartList">
+            <li className="cartListLi">
+              <div className="cartListCheckBox">
+                <input type="checkbox" />
+              </div>
+              <div className="cartListProductBox">
+                <a href="#">이미지</a>
+                <div className="cartListProductText">
+                  <p>배달이친구들 팝업카드 2종</p>
+                  <p>잘먹고 잘살자</p>
+                </div>
+              </div>
+              <div className="cartListInfoBox">
+                <div className="cartListQuantity">
+                  <button>-</button>
+                  <input type="number" />
+                  <button>+</button>
+                </div>
+                <div className="cartListPrice">
+                  <p>4000원</p>
+                </div>
+              </div>
+              <div className="cartListDeleteBox">
+                <button>❌</button>
+              </div>
+            </li>
+          </ul>
         </div>
         <div className="cartPayment">
           <div className="paymentResult">
@@ -30,8 +59,8 @@ const Cart = () => {
               <span>0원</span>
             </div>
           </div>
-          <div>
-            <button>0원 주문하기</button>
+          <div className="orderBtn">
+            <button disabled>0원 주문하기</button>
           </div>
         </div>
       </div>
