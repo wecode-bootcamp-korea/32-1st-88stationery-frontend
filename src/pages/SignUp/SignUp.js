fbf4d3ac65ab8f0f1482ed36afd51cd342ea39c9
HyunSuk <lha3421@gmail.com>
2022-05-03 16:58:19 +0900

    Refactor : sider 일부 반영

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -25,119 +25,6 @@ const SignUp = () => {
   });
   const [disabled, setDisabled] = useState(true);
 
-  const valueIdText = (
-    <>
-      {userInfo.id.length > 0 ? (
-        userInfo.id.includes("@") ? (
-          ""
-        ) : (
-          <p className="showValueFail" />
-        )
-      ) : (
-        <p className="showAlertText" />
-      )}
-    </>
-  );
-
-  const valuePwText = (
-    <>
-      {userInfo.pw.length > 0 ? (
-        userInfo.pw.length >= 8 ? (
-          ""
-        ) : (
-          <p className="showValuePwFail" />
-        )
-      ) : (
-        <p className="showAlertText" />
-      )}
-    </>
-  );
-
-  const valuePwCheckText = (
-    <>
-      {userInfo.pwCheck.length > 0 ? (
-        userInfo.pwCheck === userInfo.pw ? (
-          ""
-        ) : (
-          <p className="showValuePwCheckFail" />
-        )
-      ) : (
-        <p className="showAlertText" />
-      )}
-    </>
-  );
-
-  const valueNameCheckText = (
-    <>
-      {userInfo.name.length > 0 ? (
-        userInfo.name ? (
-          ""
-        ) : (
-          <p className="showValueFail" />
-        )
-      ) : (
-        <p className="showAlertText" />
-      )}
-    </>
-  );
-
-  const valuePhoneCheckText = (
-    <>
-      {userInfo.phone.length > 0 ? (
-        userInfo.phone ? (
-          ""
-        ) : (
-          <p className="showValueFail" />
-        )
-      ) : (
-        <p className="showAlertText" />
-      )}
-    </>
-  );
-  const valueAddressCheckText = (
-    <>
-      {userInfo.address.length > 0 ? (
-        userInfo.address ? (
-          ""
-        ) : (
-          <p className="showValueFail" />
-        )
-      ) : (
-        <p className="showAlertText" />
-      )}
-    </>
-  );
-
-  const valueDetailAddressCheckText = (
-    <>
-      {userInfo.detailAddress.length > 0 ? (
-        userInfo.detailAddress ? (
-          ""
-        ) : (
-          <p className="showValueFail" />
-        )
-      ) : (
-        <p className="showAlertText" />
-      )}
-    </>
-  );
-
-  const valueBirthCheckText = (
-    <>
-      {userInfo.birth.length > 0 ? (
-        userInfo.birth[4] === "-" &&
-        userInfo.birth[7] === "-" &&
-        userInfo.birth.length >= 10 ? (
-          ""
-        ) : (
-          <p className="showValueBirthFail" />
-        )
-      ) : (
-        <p className="showAlertText" />
-      )}
-    </>
-  );
-
   const onSubmit = e => {
     e.preventDefault();
     fetch("http://10.58.5.120:8000/users/signup", {
@@ -214,12 +101,7 @@ const SignUp = () => {
   };
 
   return (
-    <form
-      action="POST"
-      className="signUp"
-      onKeyUp={isDisabled}
-      onSubmit={onSubmit}
-    >
+    <div className="signUp">
       <h2 className="signUpTitle">회원가입</h2>
       <div className="accountContent">
         <div className="accountTitleProcess">
@@ -228,142 +110,231 @@ const SignUp = () => {
             <li className="process">01</li>
           </ul>
         </div>
-        <ul className="signUpInfoInputs">
-          <li className="signUpId">
-            <div className="inputIdBox relativeText">
-              <input
-                className={userInfo.id.includes("@") ? "successBox" : ""}
-                type="email"
-                placeholder="아이디"
-                name="id"
-                onChange={putUserInfo}
-                autoComplete="off"
-                onBlur={showAlertText}
-              />
-              {valueCheck.id && valueIdText}
-            </div>
-          </li>
-          <li className="signUpPw">
-            <div className="inputPwBox relativeText">
-              <input
-                className={userInfo.pw.length >= 8 ? "successBox" : ""}
-                type="password"
-                placeholder="비밀번호"
-                name="pw"
-                onChange={putUserInfo}
-                autoComplete="off"
-                onBlur={showAlertText}
-              />
-              {valueCheck.pw && valuePwText}
-            </div>
-            <div className="inputPwCheckBox relativeText">
-              <input
-                className={
-                  userInfo.pw !== "" && userInfo.pw === userInfo.pwCheck
-                    ? "successBox"
-                    : ""
-                }
-                type="password"
-                placeholder="비밀번호 확인"
-                name="pwCheck"
-                onChange={putUserInfo}
-                autoComplete="off"
-                onBlur={showAlertText}
-              />
-              {valueCheck.pwCheck && valuePwCheckText}
-            </div>
-          </li>
-          <li className="signUpName">
-            <div className="inputNameBox relativeText">
-              <input
-                className={userInfo.name ? "successBox" : ""}
-                type="text"
-                placeholder="이름"
-                name="name"
-                onChange={putUserInfo}
-                onBlur={showAlertText}
-                autoComplete="off"
-              />
-              {valueCheck.name && valueNameCheckText}
-            </div>
-          </li>
-          <li className="singUpPhone">
-            <div className="inputPhoneBox relativeText">
-              <input
-                className={userInfo.phone ? "successBox" : ""}
-                type="number"
-                placeholder="휴대폰 번호"
-                name="phone"
-                onChange={putUserInfo}
-                onBlur={showAlertText}
-                autoComplete="off"
-              />
-              {valueCheck.phone && valuePhoneCheckText}
-            </div>
-          </li>
-          <li className="signUpAddress">
-            <div className="inputAddressBox relativeText">
-              <input
-                className={userInfo.address ? "successBox" : ""}
-                type="text"
-                placeholder="주소"
-                name="address"
-                onChange={putUserInfo}
-                onBlur={showAlertText}
-                autoComplete="off"
-              />
-              {valueCheck.address && valueAddressCheckText}
-            </div>
-          </li>
-          <li className="signUpAddress">
-            <div className="inputAddressBox relativeText">
-              <input
-                className={userInfo.detailAddress ? "successBox" : ""}
-                type="text"
-                placeholder="상세주소"
-                name="detailAddress"
-                onChange={putUserInfo}
-                onBlur={showAlertText}
-                autoComplete="off"
-              />
-              {valueCheck.detailAddress && valueDetailAddressCheckText}
-            </div>
-          </li>
-          <li>
-            <div className="inputBirthBox">
-              <input
-                className={
+        <form
+          className="signUpInfoInputs"
+          onKeyUp={isDisabled}
+          onSubmit={onSubmit}
+        >
+          <div className="inputIdBox relativeText">
+            <input
+              className={userInfo.id.includes("@") ? "successBox" : ""}
+              type="email"
+              placeholder="아이디"
+              name="id"
+              onChange={putUserInfo}
+              autoComplete="off"
+              onBlur={showAlertText}
+            />
+            {valueCheck.id && (
+              <>
+                {userInfo.id.length > 0 ? (
+                  userInfo.id.includes("@") ? (
+                    ""
+                  ) : (
+                    <p className="showValueFail" />
+                  )
+                ) : (
+                  <p className="showAlertText" />
+                )}
+              </>
+            )}
+          </div>
+          <div className="inputPwBox relativeText">
+            <input
+              className={userInfo.pw.length >= 8 ? "successBox" : ""}
+              type="password"
+              placeholder="비밀번호"
+              name="pw"
+              onChange={putUserInfo}
+              autoComplete="off"
+              onBlur={showAlertText}
+            />
+            {valueCheck.pw && (
+              <>
+                {userInfo.pw.length > 0 ? (
+                  userInfo.pw.length >= 8 ? (
+                    ""
+                  ) : (
+                    <p className="showValuePwFail" />
+                  )
+                ) : (
+                  <p className="showAlertText" />
+                )}
+              </>
+            )}
+          </div>
+          <div className="inputPwCheckBox relativeText">
+            <input
+              className={
+                userInfo.pw !== "" && userInfo.pw === userInfo.pwCheck
+                  ? "successBox"
+                  : ""
+              }
+              type="password"
+              placeholder="비밀번호 확인"
+              name="pwCheck"
+              onChange={putUserInfo}
+              autoComplete="off"
+              onBlur={showAlertText}
+            />
+            {valueCheck.pwCheck && (
+              <>
+                {userInfo.pwCheck.length > 0 ? (
+                  userInfo.pwCheck === userInfo.pw ? (
+                    ""
+                  ) : (
+                    <p className="showValuePwCheckFail" />
+                  )
+                ) : (
+                  <p className="showAlertText" />
+                )}
+              </>
+            )}
+          </div>
+          <div className="inputNameBox relativeText">
+            <input
+              className={userInfo.name ? "successBox" : ""}
+              type="text"
+              placeholder="이름"
+              name="name"
+              onChange={putUserInfo}
+              onBlur={showAlertText}
+              autoComplete="off"
+            />
+            {valueCheck.name && (
+              <>
+                {userInfo.name.length > 0 ? (
+                  userInfo.name ? (
+                    ""
+                  ) : (
+                    <p className="showValueFail" />
+                  )
+                ) : (
+                  <p className="showAlertText" />
+                )}
+              </>
+            )}
+          </div>
+          <div className="inputPhoneBox relativeText">
+            <input
+              className={userInfo.phone ? "successBox" : ""}
+              type="number"
+              placeholder="휴대폰 번호"
+              name="phone"
+              onChange={putUserInfo}
+              onBlur={showAlertText}
+              autoComplete="off"
+            />
+            {valueCheck.phone && (
+              <>
+                {userInfo.phone.length > 0 ? (
+                  userInfo.phone ? (
+                    ""
+                  ) : (
+                    <p className="showValueFail" />
+                  )
+                ) : (
+                  <p className="showAlertText" />
+                )}
+              </>
+            )}
+          </div>
+          <div className="inputAddressBox relativeText">
+            <input
+              className={userInfo.address ? "successBox" : ""}
+              type="text"
+              placeholder="주소"
+              name="address"
+              onChange={putUserInfo}
+              onBlur={showAlertText}
+              autoComplete="off"
+            />
+            {valueCheck.address && (
+              <>
+                {userInfo.address.length > 0 ? (
+                  userInfo.address ? (
+                    ""
+                  ) : (
+                    <p className="showValueFail" />
+                  )
+                ) : (
+                  <p className="showAlertText" />
+                )}
+              </>
+            )}
+          </div>
+
+          <div className="inputAddressBox relativeText">
+            <input
+              className={userInfo.detailAddress ? "successBox" : ""}
+              type="text"
+              placeholder="상세주소"
+              name="detailAddress"
+              onChange={putUserInfo}
+              onBlur={showAlertText}
+              autoComplete="off"
+            />
+            {valueCheck.detailAddress && (
+              <>
+                {userInfo.detailAddress.length > 0 ? (
+                  userInfo.detailAddress ? (
+                    ""
+                  ) : (
+                    <p className="showValueFail" />
+                  )
+                ) : (
+                  <p className="showAlertText" />
+                )}
+              </>
+            )}
+          </div>
+          <div className="inputBirthBox relativeText">
+            <input
+              className={
+                userInfo.birth[4] === "-" &&
+                userInfo.birth[7] === "-" &&
+                userInfo.birth.length >= 10
+                  ? "successBox"
+                  : ""
+              }
+              type="text"
+              pattern="[0-9\b-]*"
+              placeholder="생일정보"
+              name="birth"
+              onChange={putUserInfo}
+              onBlur={showAlertText}
+              autoComplete="off"
+            />
+            {valueCheck.birth && (
+              <>
+                {userInfo.birth.length > 0 ? (
                   userInfo.birth[4] === "-" &&
                   userInfo.birth[7] === "-" &&
-                  userInfo.birth.length >= 10
-                    ? "successBox"
-                    : ""
-                }
-                type="text"
-                pattern="[0-9\b-]*"
-                placeholder="생일정보"
-                name="birth"
-                onChange={putUserInfo}
-                onBlur={showAlertText}
-                autoComplete="off"
-              />
-              {valueCheck.birth && valueBirthCheckText}
-              <p>생일을 정확하게 입력해주세요.(예: 2000-12-31)</p>
-              <p>만 14세 이상만 회원가입이 가능합니다.</p>
-            </div>
-          </li>
-        </ul>
+                  userInfo.birth.length >= 10 ? (
+                    ""
+                  ) : (
+                    <p className="showValueBirthFail" />
+                  )
+                ) : (
+                  <p className="showAlertText" />
+                )}
+              </>
+            )}
+            <p>생일을 정확하게 입력해주세요.(예: 2000-12-31)</p>
+            <p>만 14세 이상만 회원가입이 가능합니다.</p>
+          </div>
+          <div className="signUpBtn">
+            <button
+              type="submit"
+              disabled={disabled}
+              className={disabled ? "disabled" : "abled"}
+            >
+              가입하기
+            </button>
+          </div>
+        </form>
       </div>
-      <footer className="signUpBtn">
-        <button
-          type="submit"
-          disabled={disabled}
-          className={disabled ? "disabled" : "abled"}
-        >
-          가입하기
-        </button>
-      </footer>
-    </form>
+    </div>
   );
 };
 
